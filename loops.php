1c9452ba339b898f04970eeff7a9ac7d6c8868e2
Jure Žilić <34073245+jzilic@users.noreply.github.com>
2019-02-25 23:16:51 +0100

    creating loops in php

---
+++ loops.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Loops</title>
+</head>
+<body>
+<?php 
+
+//$array = array("Jure","Marko","Darko","Marija","Nataša");
+
+
+//foreach ($array as $loopdata) {
+//	echo  "My name is" ."   ".$loopdata. "<br>.";
+//}
+
+//echo $array[3];
+
+
+
+
+ ?>
+
+
+</body>
+</html>
